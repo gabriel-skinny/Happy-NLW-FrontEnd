0721d82fe7402099e0951b4aca27d18f020d7c9f
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2020-10-12 15:40:47 -0300

    App

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+import { FiPlus } from "react-icons/fi"
+import { Map, TileLayer } from "react-leaflet"
+import mapMarkerImg from "../images/mapMarker.svg"
+
+import "leaflet/dist/leaflet.css"
+import "../styles/pages/orphanages-map.css"
+
+function OrphanagesMap(){
+  return(
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanate no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visista :)</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <Map 
+        center={[-23.538953,-46.6426025]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff"/>
+      </Link>
+
+    </div>
+  )
+}
+
+export default OrphanagesMap
