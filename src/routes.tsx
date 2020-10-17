60f579d3cf25a16a459c64e55fd96ad1aa0f7f82
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2020-10-16 22:50:40 -0300

    App

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,14 +3,18 @@ import React from "react"
 import { BrowserRouter, Switch, Route } from "react-router-dom" 
 
 import Landing from "./pages/Landing"
-import Orphanage from "./pages/OrphanagesMap"
+import OrphanagesMap from "./pages/OrphanagesMap"
+import Orphanage from "./pages/Orphanage"
+import CreateOrphanage from "./pages/CreateOrphanage"
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing}/>
-        <Route path="/app" component={Orphanage}/>
+        <Route path="/app" component={OrphanagesMap}/>
+        <Route path="/orphanages/create" component={CreateOrphanage}/>
+        <Route path="/orphanages/:id" component={Orphanage}/>
       </Switch>
     </BrowserRouter>
   )
